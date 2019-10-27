05200eef8d9ca9da974ec161789fa40b22ba3bd9
chiarama <37699529+chiarama@users.noreply.github.com>
2019-10-27 17:24:40 -0300

    Terminado Creacion de tablas

--- Base_Datos/query.sql
+++ Base_Datos/query.sql
@@ -1,58 +1,131 @@
 use GD2C2019;
 CREATE TABLE RUBRO(
 	ID_Rubro smallint IDENTITY(1,1) PRIMARY KEY,
-	Descripcion varchar(100)
+	Descripcion nvarchar(100)
 );
 CREATE TABLE PROVEEDOR(
-	CUIT_proveedor bigint,
-	Razon_social varchar(50),
-	Domicilio varchar(100),
-	Ciudad varchar(50),
-	Telefono int,
+	CUIT_proveedor nvarchar(20),
+	Razon_social nvarchar(100),
+	Domicilio nvarchar(100),
+	Ciudad nvarchar(255),
+	Telefono numeric(18,0),
 	ID_rubro smallint,
-	Mail varchar(10),
-	Codigo_postal int,
-	Nombre_contacto varchar(50)
+	Mail nvarchar(255),
+	Codigo_postal numeric(18,0),
+	Nombre_contacto nvarchar(100)
 	PRIMARY KEY(CUIT_proveedor,Razon_Social),
 	FOREIGN KEY (ID_Rubro) REFERENCES rubro
 );
 CREATE TABLE CLIENTES(
-	DNI_cliente int,
-	Codigo_postal int,
-	Nombre varchar(50),
-	Apellido varchar(50),
-	Direccion varchar (100),
-	Telefono int,
-	Mail varchar(100),
-	Fecha_nacimiento int,
-	Ciudad varchar (50),
-	Credito dec(100000,2),
+	DNI_cliente numeric(18,0),
+	Codigo_postal numeric(18,0),
+	Nombre nvarchar(255),
+	Apellido nvarchar(255),
+	Direccion nvarchar (255),
+	Telefono numeric(18,0),
+	Mail nvarchar(255),
+	Fecha_nacimiento datetime,
+	Ciudad nvarchar (255),
+	Credito numeric(18,2),
 	Primary Key (DNI_Cliente,Codigo_postal)
 );
 CREATE TABLE TARJETA (
 	ID_tarjeta smallint identity(1,1),
 	Fecha_Vencimiento datetime,
-	Nombre_tutorial varchar(100),
-	Tipo_pago_desc char(2) check (Tipo_pago_desc IN ('TC','TD')),
-	Primary Key (ID_tarjeta,Tipo_pago_desc)
+	Nombre_tutorial nvarchar(1255),
+	Tipo_pago_desc char(2) check (Tipo_pago_desc IN ('Crédito','Débito')),
+	Primary Key (ID_tarjeta)
 );
+
 CREATE TABLE CARGA(
 	ID_carga smallint identity(1,1) Primary Key,
-	Credito dec(100000,2),
+	Credito numeric(18,2),
 	Fecha_carga datetime,
-	Tipo_pago_desc char(2) check (Tipo_pago_desc IN ('TC','TD')),
+	Tipo_pago_desc nvarchar(100) check (Tipo_pago_desc IN ('Crédito','Efectivo','Débito')),
 	ID_tarjeta smallint,
-	DNI_Cliente int,
-	Codigo_postal int,
+	DNI_Cliente numeric(18,0),
+	Codigo_postal numeric(18,0),
 	Foreign Key (DNI_Cliente,Codigo_postal) references clientes(DNI_Cliente,Codigo_postal),
-	Foreign Key (ID_tarjeta,Tipo_pago_desc) references tarjeta (ID_tarjeta,Tipo_pago_desc)
+	Foreign Key (ID_tarjeta) references tarjeta (ID_tarjeta)
 );
 CREATE TABLE TIPO_USUARIO(
 	ID_usuario int Identity(1,1) Primary Key,
-	CUIT_proveedor bigint,
-	Razon_social varchar(50),
-	DNI_cliente int,
-	Codigo_postal int,
+	CUIT_proveedor nvarchar(20),
+	Razon_social nvarchar(100),
+	DNI_cliente numeric(18,0),
+	Codigo_postal numeric(18,0),
 	FOREIGN KEY (DNI_cliente,Codigo_postal) REFERENCES clientes(DNI_cliente,Codigo_postal),
 	FOREIGN KEY (CUIT_proveedor,Razon_social) REFERENCES proveedor(CUIT_proveedor,Razon_social)
+);
+CREATE TABLE USUARIO(
+	ID_usuario int Primary Key,
+	contrasenia nvarchar(50),
+	Nombre_usuario nvarchar(50),
+	Foreign Key (ID_usuario) REFERENCES tipo_usuario
+);
+CREATE TABLE ROL(
+	ID_rol smallint Primary Key,
+	Nombre varchar(20)
+);
+CREATE TABLE USUARIO_ROL(
+	ID_usuario int,
+	ID_rol smallint,
+	PRIMARY KEY (ID_usuario,ID_rol),
+	FOREIGN KEY (ID_usuario) REFERENCES usuario,
+	FOREIGN KEY (ID_rol) REFERENCES rol
+);
+CREATE TABLE FUNCIONALIDAD(
+	ID_funcionalidad int PRIMARY KEY,
+	Descripcion varchar(100)
+);
+CREATE TABLE ROL_FUNCIONALIDAD(
+	ID_funcionalidad int,
+	ID_rol smallint,
+	PRIMARY KEY (ID_funcionalidad,ID_rol),
+	FOREIGN KEY (ID_funcionalidad) REFERENCES funcionalidad,
+	FOREIGN KEY (ID_rol) REFERENCES rol
+);
+CREATE TABLE OFERTAS(
+	Codigo_oferta nvarchar(50) PRIMARY KEY,
+	Precio_oferta numeric(18,2),
+	Fecha_publicacion datetime,
+	Fecha_vencimiento datetime,
+	Stock numeric(18,0),
+	Description nvarchar(255),
+	Precio_lista numeric(18,2),
+	Cant_maxima numeric(18,0),
+	CUIT_proveedor nvarchar(20),
+	Razon_social nvarchar(100),
+	FOREIGN KEY (CUIT_proveedor,Razon_social) REFERENCES proveedor(CUIT_proveedor,Razon_social) 
+);
+CREATE TABLE COMPRA(
+	Codigo_oferta nvarchar(50),
+	DNI_cliente numeric(18,0),
+	Codigo_postal numeric(18,0),
+	Cantidad_compra numeric(18,0),
+	Fecha_compra datetime,
+	PRIMARY KEY (Codigo_oferta,DNI_cliente, Codigo_postal),
+	FOREIGN KEY (DNI_cliente, Codigo_postal) REFERENCES clientes(DNI_cliente, Codigo_postal)
+);
+CREATE TABLE FACTURA(
+	Num_factura numeric(18,0) Primary Key,
+	Fecha_factura datetime,
+	Importe numeric (18,2),
+	DNI_cliente numeric(18,0),
+	Codigo_postal numeric(18,0),
+	Codigo_oferta nvarchar(50),
+	FOREIGN KEY (DNI_cliente,Codigo_postal) REFERENCES clientes (DNI_cliente,Codigo_postal),
+	FOREIGN KEY (Codigo_oferta) REFERENCES ofertas(Codigo_oferta)
+);
+CREATE TABLE CUPON(
+	Codigo_cupon int identity(1,1) PRIMARY KEY,
+	Entregado_fecha datetime,
+	DNI_cliente_origen numeric(18,0),
+	Codigo_postal_origen numeric(18,0), 
+	DNI_cliente_destino numeric(18,0),
+	Codigo_postal_destino numeric(18,0), 
+	Codigo_oferta nvarchar(50),
+	FOREIGN KEY (DNI_cliente_origen,Codigo_postal_origen) REFERENCES clientes (DNI_cliente,Codigo_postal),
+	FOREIGN KEY (DNI_cliente_destino,Codigo_postal_destino) REFERENCES clientes (DNI_cliente,Codigo_postal),
+	FOREIGN KEY (Codigo_oferta) REFERENCES ofertas(Codigo_oferta)
 );

004e795d2007a3c4fe00efb256a9e8a75fea624e
GonzaOrtega <gonzalonortega@gmail.com>
2019-11-10 20:09:54 -0300

    Subo base de Carga credito

---
+++ FrbaOfertas/FrbaOfertas/CargaCredito/CargarCredito.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaOfertas.CargaCredito
+{
+    public partial class CargarCredito : Form
+    {
+        public CargarCredito()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CargarCredito_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new CargarInfoTarjeta().Show();
+        }
+    }
+}
--- FrbaOfertas/FrbaOfertas/CargaCredito/CargarInfoTarjeta.cs
+++ FrbaOfertas/FrbaOfertas/CargaCredito/CargarInfoTarjeta.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FrbaOfertas.CragaCredito
+namespace FrbaOfertas.CargaCredito
 {
-    public partial class Form1 : Form
+    public partial class CargarInfoTarjeta : Form
     {
-        public Form1()
+        public CargarInfoTarjeta()
         {
             InitializeComponent();
         }

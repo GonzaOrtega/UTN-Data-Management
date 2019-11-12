f540d27d032b7a1435bb89b97a7189862c44f973
chiarama <37699529+chiarama@users.noreply.github.com>
2019-11-12 16:06:26 -0300

    Agregada interfaz de ABM ROL

--- FrbaOfertas/FrbaOfertas/AbmRol/AbmRol.cs
+++ FrbaOfertas/FrbaOfertas/AbmRol/AbmRol.cs
@@ -16,5 +16,20 @@ namespace FrbaOfertas.AbmRol
         {
             InitializeComponent();
         }
+
+        private void rOLBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.rOLBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.gD2C2019DataSet);
+
+        }
+
+        private void AbmRol_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'gD2C2019DataSet.ROL' Puede moverla o quitarla según sea necesario.
+            this.rOLTableAdapter.Fill(this.gD2C2019DataSet.ROL);
+
+        }
     }
 }

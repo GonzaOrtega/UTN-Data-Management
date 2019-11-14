89258a7d1f6e5a9c2ef88195aedad0207fa79ce3
chiarama <37699529+chiarama@users.noreply.github.com>
2019-11-14 04:48:48 -0300

    Terminado Crear

--- FrbaOfertas/FrbaOfertas/AbmProveedor/CrearProveedor.cs
+++ FrbaOfertas/FrbaOfertas/AbmProveedor/CrearProveedor.cs
@@ -21,5 +21,60 @@ namespace FrbaOfertas.AbmProveedor
         {
             Close();
         }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtCiudad.Clear();
+            txtRubro.Clear();
+            txtTelefono.Clear();
+            txtRazonSocial.Clear();
+            txtNC.Clear();
+            txtMail.Clear();
+            txtDireccion.Clear();
+            txtCUIT.Clear();
+            txtCP.Clear();
+        }
+        private bool validarPk()
+        {
+            if(proveedorTableAdapter1.GetData().Select("CUIT_proveedor like '"+txtCUIT.Text +"' and"+"Razon_social like '"+txtRazonSocial.Text+"'").Count() != 0)
+            {
+                MessageBox.Show("Lo sentimos el CUIT y la razon social ya pertenecen a otro proveedor", "ERROR!", MessageBoxButtons.OK);
+                return false;
+            }
+            return true;
+        }
+        private bool validarRubro()
+        {
+            if(rubroTableAdapter1.GetData().Select("Descripcion like '" + txtRubro.Text + "'").Count() == 0)
+            {
+                MessageBox.Show("Lo sentimos el rubro no existe", "ERROR!", MessageBoxButtons.OK);
+                return false;
+            }
+            return true;
+        }
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (this.validarPk() &&this.validarRubro) {
+                DataRow id_rubro = rubroTableAdapter1.GetData().Select("Descripcion like '" + txtRubro.Text + "'").First();
+                proveedorTableAdapter1.InsertQuery(txtCUIT.Text, txtRazonSocial.Text, txtDireccion.Text, txtCiudad.Text, Convert.ToDecimal(txtTelefono.Text),Convert.ToInt32(id_rubro["ID_rubro"].ToString()), txtMail.Text, Convert.ToDecimal(txtCP.Text), txtNC.Text);
+           
+            }
+        }
+
+        private void txtCP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtTelefono_TextChanged(object sender, EventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
     }
 }

6878309a836d267ec1ba64f56f56aa7299be1948
chiarama <37699529+chiarama@users.noreply.github.com>
2019-11-14 04:55:23 -0300

    te paso listado

--- FrbaOfertas/FrbaOfertas/AbmProveedor/ListadoProveedor.Designer.cs
+++ FrbaOfertas/FrbaOfertas/AbmProveedor/ListadoProveedor.Designer.cs
@@ -28,11 +28,234 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Nombre_rol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion_funcionalidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Habilitado_rol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Eliminar_rol = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Editar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtras.Location = new System.Drawing.Point(815, 418);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(161, 41);
+            this.btnAtras.TabIndex = 14;
+            this.btnAtras.Text = "ATRÁS";
+            this.btnAtras.UseVisualStyleBackColor = true;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrear.Location = new System.Drawing.Point(815, 371);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(161, 41);
+            this.btnCrear.TabIndex = 13;
+            this.btnCrear.Text = "CREAR";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(815, 324);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(161, 41);
+            this.btnLimpiar.TabIndex = 12;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(815, 277);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(161, 41);
+            this.btnBuscar.TabIndex = 11;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre_rol,
+            this.Descripcion_funcionalidad,
+            this.Habilitado_rol,
+            this.Eliminar_rol,
+            this.Editar});
+            this.dataGridView1.Location = new System.Drawing.Point(29, 233);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(769, 226);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Nombre_rol
+            // 
+            this.Nombre_rol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Nombre_rol.HeaderText = "Nombre_rol";
+            this.Nombre_rol.MinimumWidth = 6;
+            this.Nombre_rol.Name = "Nombre_rol";
+            this.Nombre_rol.ReadOnly = true;
+            this.Nombre_rol.ToolTipText = "NOMBRE";
+            this.Nombre_rol.Width = 111;
+            // 
+            // Descripcion_funcionalidad
+            // 
+            this.Descripcion_funcionalidad.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Descripcion_funcionalidad.HeaderText = "Descripcion_funcionalidad";
+            this.Descripcion_funcionalidad.MinimumWidth = 6;
+            this.Descripcion_funcionalidad.Name = "Descripcion_funcionalidad";
+            this.Descripcion_funcionalidad.ReadOnly = true;
+            this.Descripcion_funcionalidad.ToolTipText = "DESCRIPCIÓN";
+            this.Descripcion_funcionalidad.Width = 203;
+            // 
+            // Habilitado_rol
+            // 
+            this.Habilitado_rol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Habilitado_rol.HeaderText = "Habilitado_rol";
+            this.Habilitado_rol.MinimumWidth = 6;
+            this.Habilitado_rol.Name = "Habilitado_rol";
+            this.Habilitado_rol.ReadOnly = true;
+            this.Habilitado_rol.ToolTipText = "HABILITAR";
+            this.Habilitado_rol.Width = 124;
+            // 
+            // Eliminar_rol
+            // 
+            this.Eliminar_rol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.NullValue = "X";
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.LightCoral;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.White;
+            this.Eliminar_rol.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Eliminar_rol.HeaderText = "Eleminar_rol";
+            this.Eliminar_rol.MinimumWidth = 6;
+            this.Eliminar_rol.Name = "Eliminar_rol";
+            this.Eliminar_rol.ReadOnly = true;
+            this.Eliminar_rol.Text = "Eliminar";
+            this.Eliminar_rol.ToolTipText = "BORRAR";
+            this.Eliminar_rol.Width = 93;
+            // 
+            // Editar
+            // 
+            this.Editar.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.NullValue = "Editar";
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.LightCoral;
+            this.Editar.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Editar.HeaderText = "Editar";
+            this.Editar.MinimumWidth = 6;
+            this.Editar.Name = "Editar";
+            this.Editar.ReadOnly = true;
+            this.Editar.Text = "Editar";
+            this.Editar.ToolTipText = "EDITAR";
+            this.Editar.Width = 51;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(29, 32);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(769, 181);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Filtros de busqueda";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Habilitados",
+            "Deshabilitados",
+            "Ambos"});
+            this.comboBox1.Location = new System.Drawing.Point(23, 114);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(235, 37);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Ambos";
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textBox1.Location = new System.Drawing.Point(301, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 34);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Descripción de la funcionalidad";
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textBox2.Location = new System.Drawing.Point(23, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 34);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "Nombre del rol";
+            // 
+            // ListadoProveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(1010, 456);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ListadoProveedor";
+            this.Text = "Listar";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre_rol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion_funcionalidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Habilitado_rol;
+        private System.Windows.Forms.DataGridViewButtonColumn Eliminar_rol;
+        private System.Windows.Forms.DataGridViewButtonColumn Editar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- FrbaOfertas/FrbaOfertas/AbmProveedor/ListadoProveedor.cs
+++ FrbaOfertas/FrbaOfertas/AbmProveedor/ListadoProveedor.cs
@@ -16,5 +16,15 @@ namespace FrbaOfertas.AbmProveedor
         {
             InitializeComponent();
         }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
